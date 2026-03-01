7f2e28df32558fa7840c382344db208a640eaf26
Bhaskar2205 <166361046+Bhaskar2205@users.noreply.github.com>
2026-03-01 11:50:33 +0530

    Leader ship changes

--- sections/Team.tsx
+++ sections/Team.tsx
@@ -11,7 +11,7 @@ export default function Team() {
       image: "/team/bhaskar.jpeg",
     },
     {
-      name: "Rahul Subani",
+      name: "Rahul Subnani",
       role: "Referral Associate",
       image: "/team/rahul.jpeg",
     },
